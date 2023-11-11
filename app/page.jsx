ca5086e4a3cf2ccef538c58e1950f26dd7e01abf
Seth1001 <wobbxseth@gmail.com>
2023-11-11 15:13:41 -0800

    init dev branch - added styling, community section, fixed css contents, for transfer

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 /*
 to-do:
-- add css via modules.css and globals.css
 - test against mobile device
 - finish homepage
 - add typescript?
@@ -13,6 +12,7 @@ to-do:
 
 completed:
 - add video
+- add css via modules.css and globals.css
 - created aws s3 bucket to hold videos/images media
 - created and connected cloudfront to s3 bucket
 - updated next.config.js for cloudfront integration
@@ -22,85 +22,152 @@ export default function Home()
 {
     return (
         <div className = {styles.container}>
-            <div className = {styles.videoContainer}>
+            
+            {/* container holding wallpaper */}
+            <div className = {styles.wallpaper}>
 
-                <div className = {styles.itemDesc}>
-                    <p className = {styles.text}>punkstarr's </p>
-                    <h1 className = {styles.gameTitle}>SURVIVAL GAMES</h1>
-                    <p className = {styles.text}>battle royale is out now!</p>
-                    <Link href = "https://www.roblox.com/games/10805907537/SURVIVAL-GAMES-BATTLEPASS" className = {styles.playNowButton}>Play HERE</Link>
+                {/* container holding content */}
+                <div className = {styles.contentContainer}>
+                
+                    {/* section - video player + message */}
+                    <div className = {styles.videoContainer}>
+
+                        <div className = {styles.itemDesc}>
+                            <h1 className = {styles.nameTitle}>punkstarr</h1>
+                            <p className = {styles.text}>We're an independent Roblox game development studio.</p>
+                            <Link href = "https://www.roblox.com/games/10805907537/SURVIVAL-GAMES-BATTLEPASS" className = {styles.playNowButton}>Play HERE</Link>
+                        </div>
+
+                        <div className = {styles.itemVideo}>
+                            <video className = {styles.video} autoPlay loop muted src = "https://d3ith6uwkonxsg.cloudfront.net/videos/SG-Trailer-[BETA].mp4"/>
+                        </div>
+
+                    </div>
+                    {/* END - video player */}
+
+                    {/* section - how to play imgs + msgs */}
+                    <div className = {styles.gameDescriptionContainer}>
+
+                    <div className = {styles.sectionHeading}>
+                            <hr className = {styles.lineSeperator}/>
+                            <h3 className = "redText">SURVIVAL GAMES</h3>
+                            <hr className = {styles.lineSeperator}/>
+                        </div>
+
+                        <p className = {styles.welcomeMsg}>How to <span className = "redText">PLAY</span> our new BATTLE ROYALE!</p>
+
+                        <div className = {styles.sectionContainer}>
+                            <Image
+                                className = {styles.imageContainer}
+                                alt = "gameplay image 1"
+                                src = "https://d3ith6uwkonxsg.cloudfront.net/images/home-gameplay1.jpg"
+                                width = {1200}
+                                height = {500}
+                                style = {{
+                                    maxWidth: '35%',
+                                    height: 'auto',
+                                }}
+                            />
+
+                            <div className = {styles.leftDesc}>
+                                <h1 className = {styles.descTitle}>LOOT!</h1>
+                                <p className = {styles.descText}>Search the various maps for chests for weapons, armor, food, and more!</p>
+                                <p className = {styles.descText}>Gain XP throughout the match to enchant your items</p>
+                            </div>
+                        </div>
+
+                        <div className = {styles.sectionContainer}>
+
+                            <div className = {styles.rightDesc}>
+                                <h1 className = {styles.descTitle}>BATTLE!</h1>
+                                <p className = {styles.descText}>Search the various maps for chests for weapons, armor, food, and more!</p>
+                                <p className = {styles.descText}>Gain XP throughout the match to enchant your items</p>
+                            </div>
+
+                            <Image
+                                className = {styles.imageContainer}
+                                alt = "gameplay image 1"
+                                src = "https://d3ith6uwkonxsg.cloudfront.net/images/home-gameplay2.jpg"
+                                width = {1200}
+                                height = {500}
+                                style = {{
+                                    maxWidth: '35%',
+                                    height: 'auto',
+                                }}
+                            />
+                        </div>
+
+                        <div className = {styles.sectionContainer}>
+                            <Image
+                                className = {styles.imageContainer}
+                                alt = "gameplay image 1"
+                                src = "https://d3ith6uwkonxsg.cloudfront.net/images/home-gameplay4.jpg"
+                                width = {1200}
+                                height = {500}
+                                style = {{
+                                    maxWidth: '35%',
+                                    height: 'auto',
+                                }}
+                            />
+
+                            <div className = {styles.leftDesc}>
+                                <h1 className = {styles.descTitle}>SURVIVE!</h1>
+                                <p className = {styles.descText}>Search the various maps for chests for weapons, armor, food, and more!</p>
+                                <p className = {styles.descText}>Gain XP throughout the match to enchant your items</p>
+                            </div>
+                        </div>
+
+                    </div> 
+                    {/* END - how to play */}
+
+                    {/* section - about punkstarr */}
+                    <div className=  {styles.aboutUsContainer}>
+
+                        <div className = {styles.sectionHeading}>
+                            <hr className = {styles.lineSeperator}/>
+                            <h3 className = "redText">COMMUNITY</h3>
+                            <hr className = {styles.lineSeperator}/>
+                        </div>
+
+                        <p className = {styles.welcomeMsg}>We're creating a <span className = "redText">thriving</span> gaming community on Roblox </p>
+
+                        <div className = {styles.playSessionsCard}>
+                            <div className = {styles.cardText}>
+                                        <h2 className = "redTextTitle">100,000+</h2>
+                                        <p>Play Sessions</p>
+                            </div>
+                        </div>
+
+
+                        <div className = {styles.twoCardContainer}>
+
+                            <div className = {styles.twitterFollowersCard}>
+                                <div className = {styles.cardText}>
+                                    <h2 className = "redTextTitle">500+</h2>
+                                    <p>Twitter Followers</p>
+                                </div>
+                            </div>
+
+                            <div className = {styles.discordMembersCard}>
+                                <div className = {styles.cardText}>
+                                    <h2 className = "redTextTitle">800+</h2>
+                                    <p>Discord Members</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div> 
+                    {/* END - about us */}
+
                 </div>
-
-                <div className = {styles.itemVideo}>
-                    <video className = {styles.video} autoPlay loop muted src = "https://d3ith6uwkonxsg.cloudfront.net/videos/SG-Trailer-[BETA].mp4" />
-                </div>
-
+                {/* END - content container */}
+            
             </div>
+            {/* END - wallpaper wrapper */}
 
-            <div className = {styles.gameDescriptionContainer}>
-
-                <div className = {styles.sectionContainer}>
-                    <Image
-                        className = {styles.imageContainer}
-                        alt = "gameplay image 1"
-                        src = "https://d3ith6uwkonxsg.cloudfront.net/images/home-gameplay1.jpg"
-                        width = {1200}
-                        height = {500}
-                        style = {{
-                            maxWidth: '35%',
-                            height: 'auto',
-                        }}
-                    />
-
-                    <div className = {styles.leftDesc}>
-                        <h1 className = {styles.descTitle}>LOOT!</h1>
-                        <p className = {styles.descText}>Search the various maps for chests for weapons, armor, food, and more!</p>
-                        <p className = {styles.descText}>Gain XP throughout the match to enchant your items</p>
-                    </div>
-                </div>
-
-                <div className = {styles.sectionContainer}>
-
-                    <div className = {styles.rightDesc}>
-                        <h1 className = {styles.descTitle}>BATTLE!</h1>
-                        <p className = {styles.descText}>Search the various maps for chests for weapons, armor, food, and more!</p>
-                        <p className = {styles.descText}>Gain XP throughout the match to enchant your items</p>
-                    </div>
-
-                    <Image
-                        className = {styles.imageContainer}
-                        alt = "gameplay image 1"
-                        src = "https://d3ith6uwkonxsg.cloudfront.net/images/home-gameplay2.jpg"
-                        width = {1200}
-                        height = {500}
-                        style = {{
-                            maxWidth: '35%',
-                            height: 'auto',
-                        }}
-                    />
-                </div>
-
-                <div className = {styles.sectionContainer}>
-                    <Image
-                        className = {styles.imageContainer}
-                        alt = "gameplay image 1"
-                        src = "https://d3ith6uwkonxsg.cloudfront.net/images/home-gameplay4.jpg"
-                        width = {1200}
-                        height = {500}
-                        style = {{
-                            maxWidth: '35%',
-                            height: 'auto',
-                        }}
-                    />
-
-                    <div className = {styles.leftDesc}>
-                        <h1 className = {styles.descTitle}>SURVIVE!</h1>
-                        <p className = {styles.descText}>Search the various maps for chests for weapons, armor, food, and more!</p>
-                        <p className = {styles.descText}>Gain XP throughout the match to enchant your items</p>
-                    </div>
-                </div>
-
-            </div>
         </div>
+        // END - container
+
     );
 }
