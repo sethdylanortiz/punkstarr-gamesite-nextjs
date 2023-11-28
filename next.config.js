9b6de953d5cac39a866f3e4b8f4b8fa40bddf066
sethdylan <sethdylanortiz@gmail.com>
2023-11-27 23:13:42 -0800

    updated config file to export env var to aws amplify - for building

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+    env:{
+        ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+        SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+        REGION: process.env.REGION,
+        TABLE_NAME: process.env.TABLE_NAME,
+    },
     images: {
         formats: ['image/avif', 'image/webp'],
         domains: ['d3ith6uwkonxsg.cloudfront.net', 'y0pqvwydld.execute-api.us-west-1.amazonaws.com/StoreBugReportDynamoDB']
