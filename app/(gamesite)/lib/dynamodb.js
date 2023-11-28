2f007ed0597a70a7ad7f98048cf61c38a8746ab8
sethdylan <sethdylanortiz@gmail.com>
2023-11-27 17:37:42 -0800

    updated env var names

--- app/(gamesite)/lib/dynamodb.js
+++ app/(gamesite)/lib/dynamodb.js
@@ -3,10 +3,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const dbClient = new DynamoDBClient({
-    region: process.env.AWS_REGION,
+    region: process.env.REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey:process.env.SECRET_ACCESS_KEY,
     },
 });
 
@@ -14,14 +14,12 @@ const docClient = DynamoDBDocumentClient.from(dbClient);
 
 const writeBugDB = async(input_username, input_email, input_message) => {
 
-    // console.log(process.env.AWS_ACCESS_KEY_ID);
-
     // get current time
     const cur_time_utc = new Date();
 
     // build report message
     const params = {
-        TableName: process.env.AWS_TABLE_NAME,
+        TableName: process.env.TABLE_NAME,
         Item: {
           'timestamp': cur_time_utc.toISOString(),
           'email': input_username,
